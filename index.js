6c63a5c654dd78ffb0714921e2af0153bddbb748
lillianaleecode <lilliana.lee@code.berlin>
2022-05-07 20:39:52 +0200

    enable type:module

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+//const express = require('express') //with node, this syntax is no longer needed if we enable it. to enable it, go to package.json and add "type":"module"
