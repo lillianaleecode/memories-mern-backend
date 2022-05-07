2f330e5bffde9814ab7960c5d94cc77566c87d9f
lillianaleecode <lilliana.lee@code.berlin>
2022-05-07 21:33:45 +0200

    connect to database Mongodb Atlas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 //const express = require('express') //with node, this syntax is no longer needed if we enable it. to enable it, go to package.json and add "type":"module"
@@ -12,3 +12,9 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+//DB CONNECTION (MONGODB through MONGOOSE)
+const CONNECTION_URL = 'mongodb+srv://root:1234@node.zn6hp.mongodb.net/node?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
