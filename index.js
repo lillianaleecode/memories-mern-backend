3c10fbe275a889f22f42661a94ec45f3e5816b5e
lillianaleecode <lilliana.lee@code.berlin>
2022-05-07 21:32:31 +0200

    set basic app.use()

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+
 import cors from 'cors';
 
 //const express = require('express') //with node, this syntax is no longer needed if we enable it. to enable it, go to package.json and add "type":"module"
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
